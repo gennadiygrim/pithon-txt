8eb6fd09974e05815a317f07253bcd0d1e68af1b
gennadiygrim <gennaduygrim>
2023-01-13 20:59:04 +0300

    in master

--- taskmanager/main/urls.py
+++ taskmanager/main/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.index),
-    path('about-us',views.index)
+    path('about-us',views.about)
 ]
--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("<h4>Hello</h4>")
+    return render(request,'main/index.html')
 
 def about(request):
     return HttpResponse("<h4>About</h4>")
