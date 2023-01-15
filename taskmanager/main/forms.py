4bb08788248962b27d7ce29c08a27f73ab52a680
gennadiygrim <gennaduygrim>
2023-01-15 22:33:48 +0300

    in master djngo3

--- taskmanager/main/forms.py
+++ taskmanager/main/forms.py
@@ -1,7 +1,16 @@
 from .models import Task
-from django.forms import ModelForm
-
+from django.forms import ModelForm,TextInput,Textarea
 class TaskForm(ModelForm):
     class Meta:
         model= Task
         fields =["title","task"]
+        vidgets ={"title":Textarea(attrs={
+                'placeholder': 'Введите текст',
+                'class': 'form-control'}),
+                  
+                "task":Textarea(attrs={
+                'placeholder': 'Введите текст',
+                'class': 'form-control'})
+        }
+        
+        
